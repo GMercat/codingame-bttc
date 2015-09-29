1d2c9e95ec566d0b7fdcd8d5780f9c90aec8a726
GMercat <guillaume.mercat@gmail.com>
2015-09-29 08:15:43 +0200

    Add plateau and case

--- main.cpp
+++ main.cpp
@@ -5,12 +5,49 @@
 
 using namespace std;
 
+struct Case {
+   int mOwner;
+
+   Case (): mOwner (-1) {}
+
+   void setValue (const string& aValue) {
+      if (0 == string(".").compare(aValue)) {
+         mOwner = -1;
+      } else {
+         mOwner = std::stoi(aValue);
+      }
+   }
+
+   bool isMine() {
+      return mOwner == 0;
+   }
+
+   bool isEmpty() {
+      return mOwner == -1;
+   }
+};
+
+class Plateau {
+public:
+   Plateau () {}
+   void setLine(const int& aIndLine, const string& aLine) {
+      for (unsigned int col = 0; col < aLine.length(); ++col) {
+         mPlateau[aIndLine][col].setValue(&aLine[col]);
+      }
+   }
+
+private:
+   Case mPlateau[20][35];
+};
+
 /**
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
 int main()
 {
+   Plateau plateau;
+
     int opponentCount; // Opponent count
     cin >> opponentCount; cin.ignore();
 
@@ -31,6 +68,7 @@ int main()
         for (int i = 0; i < 20; i++) {
             string line; // One line of the map ('.' = free, '0' = you, otherwise the id of the opponent)
             cin >> line; cin.ignore();
+            plateau.setLine(i, line);
         }
 
         // Write an action using cout. DON'T FORGET THE "<< endl"
